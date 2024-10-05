9f56c6e6316f3ce5687a316fb5e5701e1933d93b
gustbaccas <gustbaccas@outlook.com>
2024-10-05 11:58:10 -0300

    criei o projeto e o header

---
+++ App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+import Header from './src/Header';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View>
+        <Header/>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+});
---
+++ src/Header/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { View, Text, StyleSheet, Image } from "react-native";
+
+export default function Header() {
+    return (
+        <View style={styles.header}>
+
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    header: {
+        height: 250,
+        backgroundColor: 'red'
+    }
+})
