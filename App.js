13f3c4a5165a254ad6b7d2cad927bc45b8cb95d8
gustbaccas <gustbaccas@outlook.com>
2024-10-21 18:15:05 -0300

    implementei o icon de esconder a senha

--- App.js
+++ App.js
@@ -1,17 +1,18 @@
 import React, { useState } from 'react'
 import { StyleSheet, Text, TextInput, View, TouchableOpacity } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 
 import Header from './src/Header';
 
 export default function App() {
   const [id, setId] = useState('')
   const [senha, setSenha] = useState('')
+  const [mostrarSenha, setMostrarSenha] = useState(false)
   const [isChecked, setIsChecked] = useState(false)
 
   const handlePress = () => {
     setIsChecked(!isChecked)
   }
-
 
   return (
     <View style={styles.container}>
@@ -34,24 +35,36 @@ export default function App() {
       <View style={styles.senha}>
         <Text style={styles.senhaText}>SENHA</Text>
 
-        <TextInput
-          style={styles.input}
-          onChangeText={setSenha}
-          value={senha}
-          placeholder='Digite a senha'
-          placeholderTextColor='#808080'
-        />
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.input}
+            onChangeText={setSenha}
+            value={senha}
+            placeholder='Digite a senha'
+            placeholderTextColor='#808080'
+            secureTextEntry={!mostrarSenha}
+          />
+          <TouchableOpacity onPress={() => setMostrarSenha(!mostrarSenha)}>
+            <Ionicons
+              name={mostrarSenha ? 'eye' : 'eye-off'}
+              size={24}
+              color="#808080"
+              style={styles.eyeIcon}
+            />
+          </TouchableOpacity>
+        </View>
+
       </View>
 
       <View style={styles.checkboxContainer}>
-      <TouchableOpacity onPress={handlePress} style={styles.checkboxRow}activeOpacity={1}>
-      <View style={[styles.checkbox, isChecked && styles.checked]}>
-        {isChecked && <Text style={styles.checkmark}>✓</Text>}
+        <TouchableOpacity onPress={handlePress} style={styles.checkboxRow} activeOpacity={1}>
+          <View style={[styles.checkbox, isChecked && styles.checked]}>
+            {isChecked && <Text style={styles.checkmark}>✓</Text>}
+          </View>
+          <Text style={styles.label}>Mantenha-me conectado</Text>
+        </TouchableOpacity>
       </View>
-      <Text style={styles.label}>Mantenha-me conectado</Text>
-    </TouchableOpacity>
-      </View>
-      
+
       <View style={styles.containerConnect}>
         <TouchableOpacity style={styles.connect}>
           <Text style={styles.connectText}>CONECTAR</Text>
@@ -84,6 +97,7 @@ const styles = StyleSheet.create({
     borderColor: '#C0C0C0',
     borderWidth: 0.5,
     padding: 10,
+    paddingRight: 40,
     color: '#808080',
     backgroundColor: '#000000',
     borderRadius: 7,
@@ -100,7 +114,16 @@ const styles = StyleSheet.create({
     paddingBottom: 5,
     letterSpacing: 1,
   },
-  checkboxContainer:{
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  eyeIcon: {
+    position: 'absolute',
+    right: 43,
+    top: 8,
+  },
+  checkboxContainer: {
     padding: 10,
     marginTop: 3,
     marginLeft: 24,
@@ -108,10 +131,10 @@ const styles = StyleSheet.create({
   checkboxRow: {
     flexDirection: 'row',
     alignItems: 'center',
-    
+
   },
   checkbox: {
-    width: 24, 
+    width: 24,
     height: 24,
     borderWidth: 0.5,
     borderColor: '#C0C0C0',
